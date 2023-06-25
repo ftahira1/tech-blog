31078b889d4c44b70fd2806612f42eddfc91d1aa
ftahira1 <123195285+ftahira1@users.noreply.github.com>
2023-06-25 10:32:30 -0400

    Completed all js for modules

--- models/user.js
+++ models/user.js
@@ -1,57 +1,45 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 class User extends Model {
-  //set up method to check password
-  checkPassword(loginPw) {
+  checkPasword(loginPw) {
     return bcrypt.compareSync(loginPw, this.password);
   }
 }
 
-User.init(
+User.init (
   {
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     username: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [5]
-      }
-    }
+        len: [6],
+      },
+    },
   },
   {
     hooks: {
-      // set up beforeCreate lifecycle hook functionality
       async beforeCreate(newUserData) {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        newUserData.password = await bcrypt.hash(newUserData.password,10);
         return newUserData;
       },
-      async beforeUpdate(updatedUserData) {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      }
-    },  
-    //pass in the imported sequelize connection
+    },
     sequelize,
-    // don't automatically create timestamp fields
     timestamps: false,
-    // don't plurallize name of database table
     freezeTableName: true,
-    // use underscores instead of camel-casing
     underscored: true,
-    // make it so our model name stays in lowercase in the database
-    modelName: 'user'
+    modelName: 'user',
   }
 );
 
