e0e6e7da484d7cd9191898478a082b4ffeb17f04
ftahira1 <123195285+ftahira1@users.noreply.github.com>
2023-06-25 12:19:08 -0400

    Completed server.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,33 @@
 const router = require('express').Router();
 
-const {User, Post, Comment} = require('../../models/user');
+const { User } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const dbUserData = await User.findAll({
+            attributes: { exlude: ['password']},
+        });
+        res.status(200).json(dbUserData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            attributes: {exlude: ['password']},
+            where: { id: req.params.id},
+            include: [
+            {
+                model: 'Post',
+                attributes: ['id', 'title', 'post_content', 'created_at']
+            },
+            {
+                model: 'Comment',
+                attributes: ['id']
+            },
+        ]        
+        })
+    }
+})
--- server.js
+++ server.js
@@ -1,15 +1,49 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
-module.exports = sequelize;
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: 'Super secret secret',
+  cookies: {
+    maxAge: 60 * 60 * 1000,
+    expires: 10 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(sess);
+
+const hbs = exphbs.create({helpers});
+
+app.engine('handlbars', hbs.engine);
+app.set('/view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => 
+  console.log(
+    `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+  )
+  );
+});
